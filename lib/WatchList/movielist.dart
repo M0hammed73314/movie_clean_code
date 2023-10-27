148a825b16907f7d3e6221e04c37e8263a263c9c
Legion <mohamed.abdou50050@gmail.com>
2023-10-27 21:53:29 +0200

    watch list

---
+++ lib/WatchList/movielist.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:movies/MyTheme.dart';
+
+class MovieList extends StatelessWidget {
+  String image;
+  String filmName;
+  String date;
+  String actor;
+
+  MovieList(
+      {required this.image,
+      required this.filmName,
+      required this.date,
+      required this.actor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Row(
+      children: [
+        Stack(children: [
+          Image.asset(image),
+          Align(
+              alignment: Alignment.topLeft,
+              child: Image.asset("assets/images/checkmark.png"))
+        ]),
+        Container(
+          padding: EdgeInsets.all(8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                filmName,
+                style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500,
+                    color: MyTheme.whiteColor),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                date,
+                style: TextStyle(
+                    fontSize: 13,
+                    color: MyTheme.greyColor,
+                    fontWeight: FontWeight.w400),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                actor,
+                style: TextStyle(
+                    fontSize: 13,
+                    fontWeight: FontWeight.w400,
+                    color: MyTheme.greyColor),
+              ),
+            ],
+          ),
+        )
+      ],
+    ));
+  }
+}
